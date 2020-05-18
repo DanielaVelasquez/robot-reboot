24b5b46c8550304d463e736206904faeb735b91c
danielavelasquez <dvelasquez@prontoforms.com>
2020-05-18 17:14:18 -0400

    Test cases to check a robot is correctly place if it founds another fellow robot on its way

--- test/models/test_robotteboot.py
+++ test/models/test_robotteboot.py
@@ -54,6 +54,23 @@ class TestRobotReboot(unittest.TestCase):
         rr.move_robot("A", "N")
         self.assertEqual((0, 0), rr.robots["A"])
 
+    def test_move_robot_north_when_another_robot_on_the_way(self):
+        maze_cells = np.array([
+            [0],
+            [0],
+            [0],
+            [0],
+            [0]
+        ])
+        maze = Maze(maze_cells)
+        robots = {
+            "A": (4,0),
+            "B": (2, 0)
+        }
+        rr = RobotReboot(maze, robots)
+        rr.move_robot("A", "N")
+        self.assertEqual((3, 0), rr.robots["A"])
+
     def test_move_robot_south_with_wall_at_north(self):
         maze_cells = np.array([
             [0],
@@ -102,6 +119,23 @@ class TestRobotReboot(unittest.TestCase):
         rr.move_robot("A", "S")
         self.assertEqual((4, 0), rr.robots["A"])
 
+    def test_move_robot_south_when_another_robot_on_the_way(self):
+        maze_cells = np.array([
+            [0],
+            [0],
+            [0],
+            [0],
+            [0]
+        ])
+        maze = Maze(maze_cells)
+        robots = {
+            "A": (0,0),
+            "B": (2, 0)
+        }
+        rr = RobotReboot(maze, robots)
+        rr.move_robot("A", "S")
+        self.assertEqual((1, 0), rr.robots["A"])
+
     def test_move_robot_east_with_wall_at_east(self):
         maze_cells = np.array([[0, 0, Maze.E, 0, 0]])
         maze = Maze(maze_cells)
@@ -132,6 +166,17 @@ class TestRobotReboot(unittest.TestCase):
         rr.move_robot("A", "E")
         self.assertEqual((0, 4), rr.robots["A"])
 
+    def test_move_robot_east_when_another_robot_on_the_way(self):
+        maze_cells = np.array([[0, 0, 0, 0, 0]])
+        maze = Maze(maze_cells)
+        robots = {
+            "A": (0,0),
+            "B": (0,2)
+        }
+        rr = RobotReboot(maze, robots)
+        rr.move_robot("A", "E")
+        self.assertEqual((0, 1), rr.robots["A"])
+
     def test_move_robot_west_with_wall_at_west(self):
         maze_cells = np.array([[0, 0, Maze.W, 0, 0]])
         maze = Maze(maze_cells)
@@ -161,6 +206,17 @@ class TestRobotReboot(unittest.TestCase):
         rr = RobotReboot(maze, robots)
         rr.move_robot("A", "W")
         self.assertEqual((0, 0), rr.robots["A"])
+
+    def test_move_robot_west_when_another_robot_on_the_way(self):
+        maze_cells = np.array([[0, 0, 0, 0, 0]])
+        maze = Maze(maze_cells)
+        robots = {
+            "A": (0,4),
+            "B": (0,2)
+        }
+        rr = RobotReboot(maze, robots)
+        rr.move_robot("A", "W")
+        self.assertEqual((0, 3), rr.robots["A"])
 
     def test_is_a_robot_on_position_on_maze(self):
         maze_cells = np.array([[0, 0, 0, 0, 0]])
