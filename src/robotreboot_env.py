40e425806b79a610722461373ab3189246e59e8b
danielavelasquez <dvelasquez@prontoforms.com>
2020-05-31 09:57:49 -0400

    Defining available actions given a RobotReboot game

--- src/robotreboot_env.py
+++ src/robotreboot_env.py
@@ -7,11 +7,8 @@ from models.robotreboot import RobotReboot
 from models.maze import Maze
 from models.robotreboot import Goal
 
-class RobotRebootEnv(gym.Env):
-    ACTIONS = ["A_N", "A_S", "A_E", "A_W",
-              "B_N", "B_S", "B_E", "B_W",
-              "C_N", "C_S", "C_S", "C_W"]
 
+class RobotRebootEnv(gym.Env):
     REWARD = 100
     PUNISHMENT = -1
 
@@ -19,7 +16,7 @@ class RobotRebootEnv(gym.Env):
         self.robot_reboot = robot_reboot
         self.reward_range = (RobotRebootEnv.PUNISHMENT, RobotRebootEnv.REWARD)
         # North, South, East or West for each robot
-        self.action_space = spaces.Discrete(4 * self.robot_reboot.count_robots)
+        self.action_space = spaces.Discrete(len(self.actions))
 
         # Observation is a three dimensional array
         obs = self.robot_reboot.state
@@ -49,6 +46,16 @@ class RobotRebootEnv(gym.Env):
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
+
+    @property
+    def actions(self):
+        actions = list()
+        for robot_id in self.robot_reboot.robots:
+            actions.append(f'{robot_id}_N')
+            actions.append(f'{robot_id}_S')
+            actions.append(f'{robot_id}_E')
+            actions.append(f'{robot_id}_W')
+        return actions
 
 
 if __name__ == "__main__":
