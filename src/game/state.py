cdea363d7bb2a4483e849ceeab727fc9f6df2d48
danielavelasquez <dvelasquez@prontoforms.com>
2021-02-07 18:21:50 -0500

    ROB-3: Clases Action, State and Game implemented

--- src/game/state.py
+++ src/game/state.py
@@ -1,15 +1,23 @@
 from abc import ABC, abstractmethod
 
+from .game import Game
+
 
 class State(ABC):
     """ State of a game
 
     Attributes:
-        sequence_id Moment in time where this state occurred
-        game        Game that contains the rules for a stat
+        sequence_id (int):  Moment in time where this state occurred
+        game (Game):        Game that the state belongs to
 
     """
-    def __init__(self, sequence_i, game):
+
+    def __init__(self, sequence_i, game: Game):
+        """ Initializes a state
+        Args:
+            sequence_i (int): Moment in time where the state occurred i.e 0 means it's how the game started
+            game (Game):      Game that the state belongs to
+        """
         self.__sequence_i = sequence_i
         self.__game = game
 
