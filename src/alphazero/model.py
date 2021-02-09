2fdb9ed5618a9b665399deb2b1b881456ff2aae9
danielavelasquez <dvelasquez@prontoforms.com>
2021-02-08 21:02:17 -0500

    ROB-9: Model class

---
+++ src/alphazero/model.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+from .state import State
+from .game import Game
+
+
+class Model(ABC):
+    """Model of a function that predicts a probability distribution among actions
+    in the states of a game
+
+    Attributes:
+        model_name (string): name of the model
+        game       (Game):   game where the model make predictions on
+    """
+
+    def __init__(self, model_name, game: Game):
+        self.__model_name = model_name
+        self.__game = game
+
+    @property
+    def model_name(self):
+        return self.__model_name
+
+    @property
+    def game(self):
+        return self.__game
+
+    @abstractmethod
+    def predict(self, state: State):
+        """Makes a probability distribution based on the state over the actions
+
+        Args:
+            state (State): where we want to predict the action with the best outcome
+        Returns:
+            p (list):   probability of best possible action from the current state
+            v (number): 1 if the game is predicted to win, 0 if it's a draw, -1 if its a lost
+        """
+        pass
+
+    @abstractmethod
+    def train(self, train_x, train_y, test_x, test_y):
+        """ Model improves its parameters to make better predictions based on training and testing data
+        Args:
+            train_x (list): data to train the model
+            train_y (list): correct label(s) for each value in train_x
+            test_x (list):  data to test how well the model is performing
+            test_y (list):  correct label(s) for each value in test_x
+
+        """
+        pass
