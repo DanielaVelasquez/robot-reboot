6e119d7be377afefe9a38a4fd98cafe980e14593
danielavelasquez <dvelasquez@prontoforms.com>
2021-02-18 21:23:40 -0500

    ROB-8: State statistics
    Statistics for a state counst visit and calculate the qualitiy of an action based on the values from the leaf state

--- src/alphazero/state_statistics.py
+++ src/alphazero/state_statistics.py
@@ -7,7 +7,7 @@ class StateStatistics:
 
     Attributes:
         n (numpy array): Total visits for each action
-        w (numpy array): Sum value(wins, lost and draws) over the leaf states of simulations for each action
+        w (numpy array): Sum values(wins, lost and draws) over the leaf states of simulations for each action
         q (numpy array): Probability of winning for each action
     """
 
@@ -16,6 +16,9 @@ class StateStatistics:
         Args:
             n_actions (number): number of actions
         """
+        assert n_actions > 0, "number of actions must be above zero"
+        assert type(n_actions) is int, "number of actions must be an integer"
+
         self.__n = np.zeros(n_actions, dtype=float)
         self.__w = np.zeros(n_actions, dtype=float)
         self.__q = np.zeros(n_actions, dtype=float)
@@ -31,3 +34,20 @@ class StateStatistics:
     @property
     def q(self):
         return self.__q
+
+    def visit(self, action_i):
+        """Register a visit from a state taking one action
+        Args:
+            action_i (int): index of the action taken
+        """
+        self.__n[action_i] += 1
+
+    def add_value(self, action_i, v):
+        """Adds the value (wins, lost and draws) from a leaf state
+        Args:
+            action_i (int): index of the action taken
+            v        (int): value of the leaf state (1 = win, 0 = draw, -1 = lost)
+        """
+        assert self.__n[action_i] != 0, f"No visits have been registered for action: {action_i}"
+        self.__w[action_i] += v
+        self.__q[action_i] = self.__w[action_i] / self.__n[action_i]
---
+++ test/alphazero/test_state_statistics.py
@@ -0,0 +1,38 @@
+import unittest
+import numpy as np
+
+from src.alphazero.state_statistics import StateStatistics
+
+
+class TestStateStatistics(unittest.TestCase):
+
+    def test_init(self):
+        StateStatistics(1)
+
+    def test_init_with_n_actions_zero(self):
+        self.assertRaises(AssertionError, lambda: StateStatistics(0))
+
+    def test_init_with_n_actions_below_zero(self):
+        self.assertRaises(AssertionError, lambda: StateStatistics(-1))
+
+    def test_init_with_n_actions_as_float(self):
+        self.assertRaises(AssertionError, lambda: StateStatistics(1.2))
+
+    def test_visit(self):
+        obj = StateStatistics(2)
+        obj.visit(0)
+        self.assertEqual(1, obj.n[0])
+
+    def test_add_value(self):
+        obj = StateStatistics(2)
+        obj.visit(0)
+        obj.add_value(0, 1)
+        obj.visit(0)
+        obj.add_value(0, 0)
+        np.testing.assert_equal([2, 0], obj.n)
+        np.testing.assert_equal([0.5, 0], obj.q)
+        np.testing.assert_equal([1, 0], obj.w)
+
+    def test_add_value_when_no_visits_registered(self):
+        obj = StateStatistics(2)
+        self.assertRaises(AssertionError, lambda: obj.add_value(0, 1))
