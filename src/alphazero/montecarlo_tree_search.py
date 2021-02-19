53f04c4c06520f1c043cdc77c58a271ebc070182
danielavelasquez <dvelasquez@prontoforms.com>
2021-02-18 19:48:35 -0500

    ROB-8: Main algorithm created for MCTS done

--- src/alphazero/montecarlo_tree_search.py
+++ src/alphazero/montecarlo_tree_search.py
@@ -8,21 +8,21 @@ class MonteCarloTreeSearch:
     """ Applies monte carlo tree search using a game player to find
     the best possible actions to take based on a state
     Attributes:
-        exploratory_parameter (number):     value [0,1] determines how important it is to explore new edges in the tree
-        max_depth             (number):     maximum depth for the tree while searching
-        game_player           (GamePlayer): player for the game to optimize moves
-        playouts              (number):     number of playouts per simulation
-        edges                 (dict):       dictionary mapping visited states with its statictis
+        heuristic_fn      (function):   function to evaluate how good is each action, receives probability and a state statistics
+        max_depth         (number):     maximum depth for the tree while searching
+        game_player       (GamePlayer): player for the game to optimize moves
+        playouts          (number):     number of playouts per simulation
+        states_statistics (dict):       dictionary between visited states and its statistics per action
     """
 
     def __init__(self, heuristic_fn, max_depth, game_player: GamePlayer, playouts=100):
         """
         Initializes a MonteCarlo tree search
         Args:
-            exploratory_parameter (number):     value [0,1] determines how important it is to explore new edges in the tree
-            max_depth             (number):     maximum depth for the tree while searching
-            game_player           (GamePlayer): player for the game to optimize moves
-            playouts              (number):     number of playouts per simulation (default 100)
+            heuristic_fn (function):   function to evaluate how good is each action, receives probability and a state statistics
+            max_depth    (number):     maximum depth for the tree while searching
+            game_player  (GamePlayer): player for the game to optimize moves
+            playouts      (number):    number of playouts per simulation (default 100)
         """
         assert heuristic_fn is not None, "heuristic_fn must be provided"
         assert max_depth > 0, "Tree depth must be greater than 0"
@@ -34,7 +34,7 @@ class MonteCarloTreeSearch:
         self.__game_player = game_player
         self.__game = self.__game_player.game
         self.__playouts = playouts
-        self.__edges = {}
+        self.__states_statistics = {}
 
     @property
     def exploratory_parameter(self):
@@ -56,7 +56,7 @@ class MonteCarloTreeSearch:
         p = np.empty(len(self.__game.actions), dtype=float)
         for i in range(len(self.__game.actions)):
             a = self.__game.actions[i]
-            next_state = self.__game_player.game.apply(a, state)
+            next_state = self.__game.apply(a, state)
             p[i] = self.__simulations(next_state)
         return p
 
@@ -69,31 +69,11 @@ class MonteCarloTreeSearch:
             return self.__game.get_value(state)
         p, v = self.__game_player.predict(state)
 
-        heuristic_values = self.__fn_heurictic_value(p, v)
-        a = best(
-            heuristic_values)  # If we make this a list then getting the righ action might get tricky, could use a map between index: action
+        stats = self.__states_statictis[state]
+        heuristic_values = self.__heuristic_fn(p, stats)
+        i_best = np.argsort(heuristic_values)[::-1][0]
+        a = self.__game.actions[i_best]
+
         next_state = self.__game.apply(a, state)
         v = self.__playouts(next_state, depth + 1)
-        self.__update_edge(state, a, v)
         return v
-
-    def __fn_heurictic_value(self, p, v):
-        """
-        returns a dictionary
-        @param p:
-        @param v:
-        @return:
-        """
-        pass
-        # TODO: pass this funciton as a parameter that can take p, v and q?
-
-    def __update_edge(self, state, action, v):
-        if state not in self.__edges:
-            self.__edges[state] = {}
-
-        if action in self.__edges[state]:
-            self.__edges[state][action].w += v
-            self.__edges[state][action].n += 1
-        else:
-            self.__edges[state][action].w = v
-            self.__edges[state][action].n = 1
