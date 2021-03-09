c996577439c919b0cde484a966536ad19b58b1ea
danielavelasquez <dvelasquez@prontoforms.com>
2021-03-08 19:36:35 -0500

    ROB-23: Mazes uploaded to code

--- src/robot_reboot/factory.py
+++ src/robot_reboot/factory.py
@@ -34,10 +34,20 @@ class RobotRebootFactory:
         state = None
         return RobotRebootGame(n_robots, maze, goal), state
 
+    def print(self, n):
+        game_configurations, n_robots = self.__get_quadrants(n)
+        for conf in game_configurations:
+            for goal in conf.houses:
+                x, y = goal.house
+                conf.quadrant[x, y] = 2
+            print(conf.quadrant)
+            print('-' * 40)
 
     def __get_quadrants(self, n):
         if n == 11:
             quadrants = self.__get_5_x_5_quadrants()
+        elif n == 31:
+            quadrants = self.__get_15x_15_quadrants()
         else:
             raise UnsupportedMazeSize(n)
         return quadrants
@@ -52,7 +62,121 @@ class RobotRebootFactory:
         n = 5
         return [
                    RobotRebootGameConfiguration(
-                       self.__build_quadrant(n, [(2, 1), (3, 2)]),
-                       [RobotRebootGoalHouse(0, (2, 2))]
+                       self.__build_quadrant(n, [(2, 1), (3, 2)]), [RobotRebootGoalHouse(0, (2, 2))]
                    )
                ], 2
+
+    def __get_15x_15_quadrants(self):
+        n = 15
+        yellow = 0
+        green = 1
+        red = 2
+        blue = 3
+        return [
+                   RobotRebootGameConfiguration(
+                       self.__build_quadrant(n, [(0, 7), (4, 11), (5, 10), (7, 4), (8, 5), (9, 0), (10, 13), (11, 2),
+                                                 (11, 14),
+                                                 (12, 1), (13, 14), (14, 13)]),
+                       [RobotRebootGoalHouse(blue, (4, 10)), RobotRebootGoalHouse(green, (8, 4)),
+                        RobotRebootGoalHouse(red, (10, 14)), RobotRebootGoalHouse(yellow, (12, 2))]
+                   ),
+                   RobotRebootGameConfiguration(
+                       self.__build_quadrant(n, [(0, 7), (2, 1), (3, 2), (3, 12), (4, 13), (8, 5), (9, 4), (9, 14),
+                                                 (10, 13),
+                                                 (11, 0), (13, 14), (14, 13)]),
+                       [RobotRebootGoalHouse(red, (2, 2)), RobotRebootGoalHouse(green, (4, 12)),
+                        RobotRebootGoalHouse(blue, (8, 4)), RobotRebootGoalHouse(yellow, (10, 14))]
+                   ),
+                   RobotRebootGameConfiguration(
+                       self.__build_quadrant(n, [(0, 5), (2, 9), (3, 10), (7, 0), (8, 7), (9, 6), (9, 12), (10, 11),
+                                                 (11, 2), (12, 3), (13, 14), (14, 13)]),
+                       [RobotRebootGoalHouse(blue, (2, 10)), RobotRebootGoalHouse(red, (8, 6)),
+                        RobotRebootGoalHouse(green, (10, 12)), RobotRebootGoalHouse(yellow, (12, 2))]
+                   ),
+                   RobotRebootGameConfiguration(
+                       self.__build_quadrant(n, [(0, 9), (3, 12), (4, 11), (5, 4), (6, 5), (7, 6), (10, 3), (11, 2),
+                                                 (13, 0), (13, 14), (14, 13)]),
+                       [RobotRebootGoalHouse(yellow, (4, 12)), RobotRebootGoalHouse(blue, (6, 4)),
+                        RobotRebootGoalHouse(green, (6, 6)), RobotRebootGoalHouse(red, (10, 2))]
+                   ),
+                   RobotRebootGameConfiguration(
+                       self.__build_quadrant(n, [(0, 9), (2, 13), (3, 2), (3, 12), (4, 1), (9, 12), (10, 13), (11, 0),
+                                                 (12, 5), (13, 6), (13, 14), (14, 13)]),
+                       [RobotRebootGoalHouse(yellow, (2, 12)), RobotRebootGoalHouse(green, (4, 2)),
+                        RobotRebootGoalHouse(blue, (10, 12)), RobotRebootGoalHouse(red, (12, 6))]
+                   ),
+                   RobotRebootGameConfiguration(
+                       self.__build_quadrant(n, [(0, 3), (1, 8), (2, 7), (3, 2), (4, 3), (6, 13), (7, 12), (11, 0),
+                                                 (12, 5), (13, 6), (13, 14), (14, 13)]),
+                       [RobotRebootGoalHouse(red, (2, 8)), RobotRebootGoalHouse(green, (4, 2)),
+                        RobotRebootGoalHouse(yellow, (6, 12)), RobotRebootGoalHouse(blue, (12, 6))]
+                   ),
+                   RobotRebootGameConfiguration(
+                       self.__build_quadrant(n, [(0, 5), (3, 12), (4, 11), (5, 10), (10, 1), (11, 2), (13, 0), (13, 8),
+                                                 (13, 14), (14, 9), (14, 13)]),
+                       [RobotRebootGoalHouse(green, (4, 10)), RobotRebootGoalHouse(yellow, (4, 12)),
+                        RobotRebootGoalHouse(red, (10, 2)), RobotRebootGoalHouse(blue, (14, 8))]
+                   ),
+                   RobotRebootGameConfiguration(
+                       self.__build_quadrant(n,
+                                             [(0, 11), (3, 6), (4, 5), (6, 9), (7, 0), (7, 4), (7, 10), (8, 5), (10, 9),
+                                              (11, 8), (13, 14), (14, 13)]),
+                       [RobotRebootGoalHouse(yellow, (4, 6)), RobotRebootGoalHouse(blue, (6, 10)),
+                        RobotRebootGoalHouse(red, (8, 4)), RobotRebootGoalHouse(green, (10, 8))]
+                   ),
+                   RobotRebootGameConfiguration(
+                       self.__build_quadrant(n, [(0, 9), (4, 13), (5, 0), (5, 12), (7, 4), (8, 5), (9, 6), (11, 10),
+                                                 (12, 9),
+                                                 (13, 14), (14, 13)]),
+                       [RobotRebootGoalHouse(blue, (4, 12)), RobotRebootGoalHouse(green, (8, 4)),
+                        RobotRebootGoalHouse(red, (8, 6)), RobotRebootGoalHouse(yellow, (12, 10))]
+                   ),
+                   RobotRebootGameConfiguration(
+                       self.__build_quadrant(n, [(0, 3), (1, 8), (2, 9), (6, 1), (7, 2), (9, 10), (10, 9), (11, 0),
+                                                 (12, 7), (13, 6), (13, 14), (14, 13)]),
+                       [RobotRebootGoalHouse(green, (2, 8)), RobotRebootGoalHouse(red, (6, 2)),
+                        RobotRebootGoalHouse(yellow, (10, 10)), RobotRebootGoalHouse(blue, (12, 6))]
+                   ),
+                   RobotRebootGameConfiguration(
+                       self.__build_quadrant(n, [(0, 9), (2, 11), (3, 12), (5, 2), (6, 3), (7, 10), (8, 9), (10, 5),
+                                                 (11, 4), (13, 0), (13, 14), (14, 13)]),
+                       [RobotRebootGoalHouse(blue, (2, 12)), RobotRebootGoalHouse(yellow, (6, 2)),
+                        RobotRebootGoalHouse(green, (8, 10)), RobotRebootGoalHouse(red, (10, 4))]
+                   ),
+                   RobotRebootGameConfiguration(
+                       self.__build_quadrant(n, [(0, 9), (2, 5), (3, 4), (6, 1), (7, 2), (7, 12), (8, 11), (9, 0),
+                                                 (11, 10), (12, 11), (13, 14), (14, 13)]),
+                       [RobotRebootGoalHouse(red, (2, 4)), RobotRebootGoalHouse(green, (6, 2)),
+                        RobotRebootGoalHouse(yellow, (8, 12)), RobotRebootGoalHouse(blue, (12, 10))]
+                   ),
+                   RobotRebootGameConfiguration(
+                       self.__build_quadrant(n, [(0, 3), (1, 6), (2, 5), (6, 13), (7, 12), (8, 1), (9, 2), (11, 8),
+                                                 (12, 9),
+                                                 (13, 0), (13, 14), (14, 13)]),
+                       [RobotRebootGoalHouse(green, (2, 6)), RobotRebootGoalHouse(yellow, (6, 12)),
+                        RobotRebootGoalHouse(red, (8, 2)), RobotRebootGoalHouse(blue, (12, 8))]
+                   ),
+                   RobotRebootGameConfiguration(
+                       self.__build_quadrant(n, [(0, 11), (5, 2), (6, 1), (8, 13), (9, 12), (11, 0), (11, 4), (12, 5),
+                                                 (13, 6), (13, 14), (14, 13)]),
+                       [RobotRebootGoalHouse(red, (6, 2)), RobotRebootGoalHouse(yellow, (8, 12)),
+                        RobotRebootGoalHouse(green, (12, 4)), RobotRebootGoalHouse(blue, (12, 6))]
+                   ),
+                   RobotRebootGameConfiguration(
+                       self.__build_quadrant(n, [(0, 9), (1, 4), (2, 3), (6, 11), (7, 12), (9, 0), (9, 8), (10, 9),
+                                                 (12, 3), (13, 2), (13, 14), (14, 13)]),
+                       [RobotRebootGoalHouse(yellow, (2, 4)), RobotRebootGoalHouse(blue, (6, 12)),
+                        RobotRebootGoalHouse(red, (10, 8)), RobotRebootGoalHouse(green, (12, 2))]
+                   ),
+                   RobotRebootGameConfiguration(
+                       self.__build_quadrant(n, [(0, 7), (2, 11), (3, 10), (4, 1), (5, 2), (7, 0), (7, 12), (8, 11),
+                                                 (11, 4), (12, 5), (13, 14), (14, 13)]),
+                       [RobotRebootGoalHouse(green, (2, 10)), RobotRebootGoalHouse(red, (4, 2)),
+                        RobotRebootGoalHouse(yellow, (8, 12)), RobotRebootGoalHouse(blue, (12, 4))]
+                   )
+               ], 4
+
+
+if __name__ == '__main__':
+    factory = RobotRebootFactory()
+    factory.print(31)
